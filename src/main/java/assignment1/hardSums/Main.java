297f2853a62f01e67a2077cb430308e7eafd2482
WuZhuoran <munoliver007@gmail.com>
2017-05-18 23:12:26 -0400

    feat: add hard sum solutios

--- src/main/java/assignment1/hardSums/Main.java
+++ src/main/java/assignment1/hardSums/Main.java
@@ -1,5 +1,7 @@
 package assignment1.hardSums;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author Oliver <a>http://poj.org/problem?id=2140</a>
@@ -7,8 +9,28 @@ package assignment1.hardSums;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+		int num = scanner.nextInt();
+		int sum = 1;
+
+		for (int i = 0; i < num / 2; i++) {
+			int count = 0;
+			for (int j = i; j < (num / 2) + 1; j++) {
+				count += j;
+				if (count == num) {
+					sum++;
+					break;
+				}
+				if (count > num) {
+					break;
+				}
+			}
+		}
+		
+		System.out.println(sum);
+
+		scanner.close();
 	}
 
 }
